299df270acb9284c9b439fe0175b503633eb7330
Ezabulbary <ezabul.bary@gmail.com>
2022-05-23 16:18:20 +0600

    bannar added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,10 @@
 import React from 'react';
+import Bannar from './Bannar';
 
 const Home = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={'https://i.ibb.co/y4C5GVD/12-sports-gear.png'} class="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
-                </div>
-            </div>
+        <div>
+            <Bannar></Bannar>
         </div>
     );
 };
