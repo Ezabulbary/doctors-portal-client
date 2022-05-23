ce27ba698da4bb48f8360cbd5ed58ebb3219485e
Ezabulbary <ezabul.bary@gmail.com>
2022-05-23 14:57:43 +0600

    doctors portel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Footer from './pages/Shared/Footer';
+import NavBar from './pages/Shared/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
