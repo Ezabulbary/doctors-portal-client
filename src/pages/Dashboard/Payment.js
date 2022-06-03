1eb553a601e319c5d4b62a573144d91963d5b8ae
Ezabulbary <ezabul.bary@gmail.com>
2022-06-03 21:30:14 +0600

    payment method

--- src/pages/Dashboard/Payment.js
+++ src/pages/Dashboard/Payment.js
@@ -1,7 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+
+const stripePromise = loadStripe('pk_test_51L6ZfSFK5FDe5DbuGUcsdzOGcyJVlENgvxJY6K7OVd9ykrlO99P928zUYYXqzkr4y5C4gZK6fGwseaRJ2MeG4i4U008gA7X3jL');
 
 const Payment = () => {
     const {id} = useParams();
@@ -30,9 +36,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
-                    {/* <Elements stripe={stripePromise}>
+                    <Elements stripe={stripePromise}>
                         <CheckoutForm appointment={appointment} />
-                    </Elements> */}
+                    </Elements>
                 </div>
             </div>
         </div>
